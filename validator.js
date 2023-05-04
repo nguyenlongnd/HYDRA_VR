a4c02b186974dfca27ee07f63454d9e0f6f2eda4
nguyen long <nguyenduclonghust@gmail.com>
2023-05-04 16:43:05 +0700

    done

--- validator.js
+++ validator.js
@@ -1,16 +1,28 @@
 
 function validator(options){
     var formElement = document.querySelector(options.form)
-    var inputElement = formElement.querySelector(options.rules.selector)
-    console.log(inputElement);
+    options.rules.forEach((rule) => {
+        var inputElement = formElement.querySelector(rule.selector)
+        var errorElement = inputElement.parentElement.querySelector(".form-message")
+        inputElement.onblur = () => {
+            var errorMessage = rule.test(inputElement.value)
+            if(errorMessage){
+                errorElement.innerText = errorMessage;
+                inputElement.parentElement.classList.add("invalid")
+            } else {
+                errorElement.innerText = "";
+                inputElement.parentElement.classList.remove("invalid")
+            }
+        }
+    })
 
 }
 
 validator.isRequired = (selector) => {
     return {
         selector,
-         test: () => {
-        
+         test: (value) => {
+            return value ? "" : "vui lòng nhập trường này"
         }
     }
 }
@@ -18,8 +30,17 @@ validator.isRequired = (selector) => {
 validator.isEmail = (selector) => {
     return {
         selector,
-        test: () => {
-
+        test: (value) => {
+            return  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value) ? "" : "vui lòng nhập đúng trường này";
+        }
+    }
+}
+validator.isPhoneNumber = (selector) => {
+    return{
+        selector,
+        test: (value) => {
+            console.log(typeof(value));
+            return /((024|0282|09|03|07|08|05)+([0-9]{8})\b)/g.test(value) ? "" : "vui lòng nhập đúng trường này"
         }
     }
 }
